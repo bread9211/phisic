132b79c6a41f7afc2f1f977fcbc33d069fdc9f3f
sunbird <sunnyfang@gmail.com>
2022-09-23 21:22:16 -0700

    message

--- main.lua
+++ main.lua
@@ -10,6 +10,7 @@ local instances = {}
 local instanceCount = 0
 
 local time = os.clock()
+local checked = {}
 
 local Vector2 = {}
 Vector2.__index = Vector2
@@ -78,6 +79,21 @@ function Ball:new(x, y, density, radius, airDrag, elasticity, groundFriction)
 end
 
 function Ball:update()
+    for _, v in ipairs(instances) do
+        if (self.vecPos-v.vecPos).Magnitude() < (self.properties.radius+v.properties.radius) and (self ~= v) and !(checked[v]==1) then
+            local normal = self.vecPos - v.vecPos
+            local reflectionVX = self.vecVel.x - normal.x*(2*self.vecVel.x*normal.x)
+            local reflectionVY = self.vecVel.y - normal.y*(2*self.vecVel.x*normal.y)
+            window.console:log(reflectionVX .. " " .. reflectionVY)
+            self.vecVel.x = self.vecVel.x + reflectionVX*10
+            self.vecVel.y = self.vecVel.y + reflectionVY*10
+
+            checked[v] = 1
+        else
+            checked[v] = nil
+        end
+    end
+
     self.vecPos.x = self.vecPos.x + self.vecVel.x
     self.vecPos.y = self.vecPos.y + self.vecVel.y
 
@@ -95,9 +111,6 @@ function Ball:update()
         self.vecVel.x = -(self.vecVel.x * self.properties.elasticity)
     end
 
-    for _, v in ipairs(instances) do
-        
-    end
 
     self.vecVel.y = self.vecVel.y + 0.98
 
@@ -125,8 +138,8 @@ local function updateMouse(_, e)
     local x = e.clientX - rect.left
     local y = e.clientY - rect.top
 
-    local newBall = Ball:new(x, y, 1, math.random(5,15), 0.99, 0.8, 0.98)
-    instances[instanceCount] = newBall
+    local newBall = Ball:new(x, y, 1, 5, 0.99, 0.5, 0.98)
+    instances[instanceCount+1] = newBall
     instanceCount = instanceCount + 1
 
     -- window.console:log("clock "..instanceCount)
